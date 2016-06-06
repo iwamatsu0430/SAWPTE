c19e71a850b7a32d9569d2123a58df4009954a0f
iwamatsu0430 <iwamatsu0430@gmail.com>
2016-06-07 00:54:57 +0900

    fix a few

--- app/assets/javascripts/control.js
+++ app/assets/javascripts/control.js
@@ -2,9 +2,13 @@ const SAWPTE    = require('./sawpte.js');
 const logger    = require('../../../lib/logger.js');
 const Logger    = logger.logger;
 const ColorCode = logger.colorCode;
+const ControlStatus = {
+  waiting: 'waiting...',
+  connecting: target => `connecting to ${target}`
+}
 const ConnectTo = {
-  local: "local",
-  global: "global"
+  local: 'local',
+  global: 'global'
 }
 
 class SAWPTEControl extends SAWPTE {
@@ -14,6 +18,9 @@ class SAWPTEControl extends SAWPTE {
 
     // presentation
     this.$status = this.$('.sawpte-control-status')[0];
+    this.$controlSeconds = this.$('.sawpte-control-seconds')[0];
+    this.$controlTitle = this.$('.sawpte-control-title')[0];
+    this.$controlPresenter = this.$('.sawpte-control-presenter')[0];
 
     // connect
     let connectTos = this.$('#sawpte-control-connect > input[name="sawpte-control-connect-radio"]');
@@ -22,6 +29,13 @@ class SAWPTEControl extends SAWPTE {
     this.$connectToServer = this.$('#sawpte-control-connect > .sawpte-control-connect-server')[0];
     this.$connectUpdateButton = this.$('#sawpte-control-connect > .sawpte-control-connect-update')[0];
 
+    // presentation
+    this.status = ControlStatus.waiting;
+    this.controlSeconds = 0;
+    this.controlTitle = '';
+    this.controlPresenter = '';
+
+    // connect
     this.connectTo = ConnectTo.local; // TODO: load loacl strage settings
     this.connectToServer = "http://localhost:9000/"; // TODO: load loacl strage settings
 
@@ -51,6 +65,10 @@ class SAWPTEControl extends SAWPTE {
 
   update() {
     // presentation
+    this.$status.setValue(this.status);
+    this.$controlSeconds.setValue(this.controlSeconds);
+    this.$controlTitle.setValue(this.controlTitle);
+    this.$controlPresenter.setValue(this.controlPresenter);
 
     // connect
     this.$connectToServer.setValue(this.connectToServer);
